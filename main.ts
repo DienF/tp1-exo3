0900cdebdd230a4063d472a7736d94b4867c2d10
DienF <68037919+DienF@users.noreply.github.com>
2020-07-30 14:56:56 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
-let x = randint(0, 4)
-let y = 0
+let ligne_inversée = 0
 basic.forever(function () {
-    led.plot(x, y)
-    basic.pause(1000)
-    led.plotBrightness(x, y, 170)
-    y += 1
-    led.plot(x, y)
-    basic.pause(1000)
-    led.plotBrightness(x, y, 170)
+    if (true) {
+        led.plot(randint(0, 4), 0)
+        basic.pause(500)
+    }
+    for (let colonne = 0; colonne <= 4; colonne++) {
+        for (let ligne = 0; ligne <= 4; ligne++) {
+            ligne_inversée = 4 - ligne_inversée
+        }
+        if (led.point(colonne, ligne_inversée) && false) {
+            led.plotBrightness(colonne, ligne_inversée, 170)
+            led.plotBrightness(colonne, ligne_inversée, 255)
+        } else if (led.pointBrightness(colonne, ligne_inversée) == 170) {
+        	
+        } else if (led.pointBrightness(colonne, ligne_inversée) == 85) {
+        	
+        } else {
+        	
+        }
+    }
 })
